9ec3ebd433101ee562a484f43fa10004c6d0de96
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-08-20 12:02:04 +0300

    Cleared

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,18 +15,6 @@ galleryRef.insertAdjacentHTML('afterbegin', elements);
 
 galleryRef.addEventListener('click', onImageClick);
 
-// -----------------------------------------------------------------------------------------------------------------
-//  Version 03
-// const instance = basicLightbox.create(`<div id="content"></div>`, {
-//   onShow: instance => {
-//     document.onkeyup = event => {
-//       onClose(event, instance);
-//     };
-//   },
-//   onClose: () => (document.onkeyup = null),
-// });
-// -----------------------------------------------------------------------------------------------------------------
-
 const instance = basicLightbox.create(`<img id="content" src='' width="800" height="600">`, {
   onShow: instance => {
     document.onkeyup = event => {
@@ -36,59 +24,15 @@ const instance = basicLightbox.create(`<img id="content" src='' width="800" heig
   onClose: () => (document.onkeyup = null),
 });
 
-// const instance = basicLightbox.create(galleryRef.querySelector('img'), {
-//     onShow: instance => {
-//       document.onkeyup = event => {
-//         onClose(event, instance);
-//       };
-//     },
-//     onClose: () => (document.onkeyup = null),
-//   });
-
 function onImageClick(event) {
   event.preventDefault();
   if (event.target.tagName !== 'IMG') {
     return;
   }
 
-  // -----------------------------------------------------------------------------------------------------------------
-  //  Version 02
-  // const instance = basicLightbox.create(
-  //   `<img id="content" src=${event.target.dataset.source} width="800" height="600">`,
-  //   {
-  //     onShow: instance => {
-  //       document.onkeyup = event => {
-  //         onClose(event, instance);
-  //       };
-  //     },
-  //     onClose: () => (document.onkeyup = null),
-  //   }
-  // );
-
   instance.element().querySelector('img').src = event.target.dataset.source;
 
   instance.show();
-
-  // -----------------------------------------------------------------------------------------------------------------
-
-  // -----------------------------------------------------------------------------------------------------------------
-  //  Version 03
-  // const modalContent = document.querySelector('#content');
-  // modalContent.innerHTML = `<img src=${event.target.dataset.source} width="800" height="600">`;
-  // -----------------------------------------------------------------------------------------------------------------
-
-  // -----------------------------------------------------------------------------------------------------------------
-  //  Version 01
-
-  //   function onModalClose(event) {
-  //     if (event.code === 'Escape') {
-  //       instance.close();
-  //       window.removeEventListener('keydown', onModalClose);
-  //     }
-  //   }
-
-  //   window.addEventListener('keydown', onModalClose);
-  // -----------------------------------------------------------------------------------------------------------------
 }
 
 //  Part for Version 02:
@@ -96,5 +40,4 @@ function onClose(event, instance) {
   if (event.code === 'Escape') {
     instance.close();
   }
-  //   console.log('Clicking...', event.code);
 }
